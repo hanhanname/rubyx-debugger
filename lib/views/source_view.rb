b76fe1645e6fe16e59128a64c2dde509a0542c95
Torsten Ruger <torsten@villataika.fi>
2015-10-29 16:53:46 +0200

    separate ticker for internal codes

--- lib/views/source_view.rb
+++ lib/views/source_view.rb
@@ -7,22 +7,20 @@ class SourceView < ElementView
 
   def draw
     @text = div
-    @element = div(".source_view") << div("h4" ,"Future") << @text
+    @ticker = div
+    @element = div(".source_view") << div("h4" ,"Future") << @ticker << @text
     @element
   end
 
   def instruction_changed
-    @text.text = source
-  end
-
-  def source
     i = @interpreter.instruction
     return "" unless i
     case i.source
     when AST::Node
-      i.source.to_s
+      @text.text = i.source.to_s
+      @ticker.text = ""
     when String
-      "String " + i.source
+      @ticker.text = i.source
     else
       raise i.source.class.name
     end
