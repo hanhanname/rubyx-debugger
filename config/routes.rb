7a8e308665f65296a8d2364ae25096e68873055f
Torsten Ruger <torsten@villataika.fi>
2018-04-17 16:35:32 +0300

    dump dead code
    some volt stuff still lying around

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-# See https://github.com/voltrb/volt#routes for more info on routes
-
-client '/about', action: 'about'
-
-# The main route, this should be last. It will match any params not
-# previously matched.
-client '/', {}
